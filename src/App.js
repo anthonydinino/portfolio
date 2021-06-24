d6e7f01f0426f161eff2b29b10ff6c22e24f1e5c
anthonydinino <adinino1996@gmail.com>
2021-06-25 02:51:21 +0930

    Added the header with mobile responsiveness and social links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import Header from "./Header";
+import Header from "./components/Header";
+import "./fonts.css";
+import Content from "./Content";
+import Footer from "./Footer";
+import React, { useEffect, useState } from "react";
+
+export const AppContext = React.createContext();
+
 function App() {
+  //check if app is using mobile view
+  const [state, setState] = useState({ isMobileView: false });
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState(() => ({ isMobileView: true }))
+        : setState(() => ({ isMobileView: false }));
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
   return (
-    <>
+    <AppContext.Provider value={state}>
       <Header />
-    </>
+      <Content />
+      <Footer />
+    </AppContext.Provider>
   );
 }
 
---
+++ src/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const Content = () => {
+  const useStyles = makeStyles({});
+  return <main></main>;
+};
+
+export default Content;
