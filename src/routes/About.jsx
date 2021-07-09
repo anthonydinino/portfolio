9f6e746974f8926472de53ac54f43a442cb99c81
anthonydinino <adinino1996@gmail.com>
2021-07-09 19:53:01 +0930

    added 2 sections to about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -12,14 +12,163 @@ import { AppContext } from "../App";
 const About = () => {
   const { navBarHeight, isDesktop } = useContext(AppContext);
   const useStyles = makeStyles({
-    grid: {},
     image: {
       borderRadius: "8px",
       width: isDesktop ? "70%" : "100%",
       minHeight: "20rem",
     },
+    imageIcon: {
+      height: "100%",
+      width: "100%",
+    },
   });
-  const { image, grid } = useStyles();
+  const { image, imageIcon } = useStyles();
+  const qualifications = (
+    <>
+      <Grid item xs={12} sm={6}>
+        <Grid container justify="center">
+          <CardMedia
+            component="img"
+            className={image}
+            image={`${process.env.PUBLIC_URL}/assets/graduation.jpg`}
+            title="Anthony graduating from UniSA"
+          />
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="primary"
+              component="p"
+              align="justify"
+            >
+              Graduating with a Bachelor of Information Technology at the
+              University of South Australia
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography
+          variant={isDesktop ? "h4" : "h6"}
+          style={{ margin: "0 0 1ch 0" }}
+        >
+          Qualifications
+        </Typography>
+        <Typography>
+          Anthony is a Web Developer and graduate from the University of South
+          Australia. Graduating with a Bachelor of Information Technology in
+          2019 and achieving a High Distinction in Web Development. He loves
+          learning new cutting edge web frameworks easily adapting to new
+          technologies. <br /> <br /> With years of experience coordinating
+          large projects and developing robust websites, his background in
+          information technology, web development, and project management inform
+          his mindful but competitive approach.
+          <br /> <br /> Anthony is forever learning something new, eager to both
+          build on his academic foundations in web development and stay in tune
+          with the latest coding practices through continued coursework and
+          professional development.
+        </Typography>
+      </Grid>
+    </>
+  );
+  const experience = (
+    <>
+      <Grid item xs={12} sm={6}>
+        <Typography
+          variant={isDesktop ? "h4" : "h6"}
+          style={{ margin: "0 0 1ch 0" }}
+        >
+          Experience
+        </Typography>
+        <Typography>
+          After graduating from UniSA, Anthony was eager to utilize his newly
+          aquired skills and add some value into the real-world. He eventually
+          was offered a job in 2020 as a project coordinator in a company called
+          2Excel
+          <br /> <br />
+          Anthony's responsibility of this job was to deliver client projects
+          starting from the brainstorming phase all the way until delivery and
+          maintainence. He was responsible for the scoping document which
+          outlined all requirements, risks and technologies used aswell as
+          designing all the barebones UI using UXPin.
+          <br /> <br /> His last responsibility was to relay that information to
+          a team of developers whilst also ensuring the project is on track and
+          can be delivered on time using Microsoft DevOps, testing for bugs
+          along the way.
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Grid container justify="center">
+          <CardMedia
+            component="img"
+            className={image}
+            image={`${process.env.PUBLIC_URL}/assets/experience.png`}
+            title="Anthony Dinino on the job at 2Excel with fellow co-worker Alexander Selman"
+          />
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="primary"
+              component="p"
+              align="justify"
+            >
+              Anthony Dinino on the job at 2Excel with fellow co-worker
+              Alexander Selman
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </>
+  );
+  const whatnow = (
+    <>
+      <Grid item xs={12} sm={6}>
+        <Typography
+          variant={isDesktop ? "h4" : "h6"}
+          align="center"
+          style={{ margin: "0 0 1ch 0" }}
+        >
+          Where to now?
+        </Typography>
+        <Typography>
+          With experience in the IT industry since 2015 and a Bachelor's Degree
+          in Information Technology, Anthony is taking a proactive approach to
+          helping businesses independently. Anthony is constantly learning the
+          most practical and cutting edge technologies to help deliver the most
+          future-proof yet functional solution. Anthony has worked with
+          technologies used for front-end development as well as back-end
+          development.
+        </Typography>
+        <Typography
+          variant={isDesktop ? "h6" : "h6"}
+          align="center"
+          style={{ margin: "2ch 0" }}
+        >
+          Technologies Learnt
+        </Typography>
+      </Grid>
+      <Grid container>
+        <Grid item xs={6} sm={3}>
+          <Box display="flex" flexDirection="column">
+            <img
+              className={imageIcon}
+              src={`${process.env.PUBLIC_URL}/assets/react.png`}
+              alt="Anthony Dinino on the job at 2Excel with fellow co-worker Alexander Selman"
+            />
+            <CardContent>
+              <Typography
+                variant="body2"
+                color="primary"
+                component="p"
+                align="center"
+              >
+                React.js
+              </Typography>
+            </CardContent>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
   return (
     <main
       style={{
@@ -31,58 +180,10 @@ const About = () => {
         <Typography variant={isDesktop ? "h2" : "h4"}>
           Who is Anthony?
         </Typography>
-        <Grid
-          container
-          spacing={3}
-          className={grid}
-          justify="center"
-          alignItems="center"
-        >
-          <Grid item xs={12} sm={6}>
-            <Typography
-              variant={isDesktop ? "h4" : "h6"}
-              style={{ margin: "0 0 1ch 0" }}
-            >
-              Qualifications
-            </Typography>
-            <Typography>
-              Anthony is a Web Developer and graduate from the University of
-              South Australia. Graduating with a Bachelor of Information
-              Technology in 2019. He loves learning new web frameworks and
-              easily adapts to new technologies. <br /> <br /> With years of
-              experience coordinating large projects and developing robust
-              websites, his background in information technology, web
-              development, and project management inform his mindful but
-              competitive approach.
-              <br /> <br /> Anthony is fueled by his passion for understanding
-              the nuances of computer science and web development. He is forever
-              learning something new, eager to both build on his academic
-              foundations in web development and programming and stay in tune
-              with the latest coding practices through continued coursework and
-              professional development.
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Grid container justify="center">
-              <CardMedia
-                component="img"
-                className={image}
-                image={`${process.env.PUBLIC_URL}/assets/graduation.jpg`}
-                title="Anthony graduating from UniSA"
-              />
-              <CardContent>
-                <Typography
-                  variant="body2"
-                  color="primary"
-                  component="p"
-                  align="justify"
-                >
-                  Graduating with a Bachelor of Information Technology at the
-                  University of South Australia
-                </Typography>
-              </CardContent>
-            </Grid>
-          </Grid>
+        <Grid container spacing={3} justify="center" alignItems="center">
+          {qualifications}
+          {experience}
+          {whatnow}
         </Grid>
       </Box>
     </main>
