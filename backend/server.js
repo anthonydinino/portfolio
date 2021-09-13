b9ece06977c88206ecea54f29bd9c5bce15652c9
anthonydinino <adinino1996@gmail.com>
2021-09-13 17:15:32 +0930

    changed default port number

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { sendMail } = require("./nodemailer");
 
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //import router
 const projectRouter = require("./projects");
